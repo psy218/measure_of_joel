a6fece4fa9eb79609aa797ecae5be15a69fe74ac
Sue Song <sue.song@mail.utoronto.ca>
2021-09-20 20:16:35 -0400

    r2 w/ bethany

--- r_function/bar_graph.R
+++ r_function/bar_graph.R
@@ -13,16 +13,17 @@ bar_graph = function(dataset, variable, ...) {
     geom_text(aes(label = paste0(prop,"%")),
               position = position_dodge(.8),
               vjust = -0.25) +
-    scale_x_discrete("",
-                     labels = stringr::str_to_title) +
+    scale_x_discrete("") +
     # scale_y_continuous("",
     #                    breaks = unique(dataset$n)) +
     scale_y_continuous("Number of Votes",
                        limits = c(0, max(dataset$n) + 1),
                        n.breaks = max(dataset$n) + 1) + 
-    viridis::scale_fill_viridis("",
-                               guide = "none",
-                                discrete = T,
-                                begin = .2) +
+    scale_fill_manual(guide = "none",
+                      values = cbPalette) + 
+    # viridis::scale_fill_viridis("",
+    #                            guide = "none",
+    #                             discrete = T,
+    #                             begin = .2) +
     theme_classic() 
 }
